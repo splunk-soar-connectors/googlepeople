2caaf5d2f5dd055883819c9d11f86a97c0ae0f17
achandya <achandya@splunk.com>
2021-12-24 14:06:28 +0530

    Address liniting failures

--- googlepeople_connector.py
+++ googlepeople_connector.py
@@ -556,9 +556,7 @@ if __name__ == '__main__':
             headers['Referer'] = login_url
 
             print("Logging into Platform to get the session id")
-            r2 = requests.post(    # nosemgrep: python.requests.best-practice.use-timeout.use-timeout
-                login_url, verify=False,
-                data=data, headers=headers)
+            r2 = requests.post(login_url, verify=False, data=data, headers=headers)   # nosemgrep
             session_id = r2.cookies['sessionid']
         except Exception as e:
             print("Unable to get session id from the platform. Error: " + str(e))
