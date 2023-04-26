a87db745155e2941360d947d44186c155da71f8f
Ronald Gil <rgil@splunk.com>
2023-04-26 14:58:15 -0700

    Fix tests

--- googlepeople_consts.py
+++ googlepeople_consts.py
@@ -19,20 +19,20 @@ GOOGLE_PROFILE_SCOPE = 'https://www.googleapis.com/auth/userinfo.profile'
 OTHER_CONTACTS_RESOURCE_NAME_PREFIX = 'otherContacts/'
 
 # Constants relating to '_get_error_message_from_exception'
-ERR_CODE_MSG = "Error code unavailable"
-ERR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
-PARSE_ERR_MSG = "Unable to parse the error message. Please check the asset configuration and|or action parameters"
+ERROR_CODE_MESSAGE = "Error code unavailable"
+ERROR_MESSAGE_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
+PARSE_ERROR_MESSAGE = "Unable to parse the error message. Please check the asset configuration and|or action parameters"
 
 # Constants relating to '_validate_integer'
-INVALID_INTEGER_ERR_MSG = "Please provide a valid integer value in the {}"
-INVALID_NON_ZERO_NON_NEGATIVE_INTEGER_ERR_MSG = "Please provide a valid non-zero positive integer value in the {}"
-INVALID_COMMA_SEPARATED_ERR_MSG = "Please provide valid comma-seprated value in the '{}' action parameter"
+INVALID_INTEGER_ERROR_MESSAGE = "Please provide a valid integer value in the {}"
+INVALID_NON_ZERO_NON_NEGATIVE_INTEGER_ERROR_MESSAGE = "Please provide a valid non-zero positive integer value in the {}"
+INVALID_COMMA_SEPARATED_ERROR_MESSAGE = "Please provide valid comma-seprated value in the '{}' action parameter"
 
 LIMIT_KEY = "'limit' action parameter"
 
-GOOGLE_CREATE_CLIENT_FAILED_MSG = "Failed to create the Google People client"
-GOOGLE_LIST_OTHER_CONTACTS_FAILED_MSG = "Failed to list other contacts"
-GOOGLE_LIST_PEOPLE_FAILED_MSG = "Failed to list people"
-GOOGLE_LIST_DIRECTORY_FAILED_MSG = "Failed to list directory"
-GOOGLE_COPY_CONTACT_FAILED_MSG = "Failed to copy contact"
-GOOGLE_GET_USER_PROFILE_FAILED_MSG = "Failed to get user profile"
+GOOGLE_CREATE_CLIENT_FAILED_MESSAGE = "Failed to create the Google People client"
+GOOGLE_LIST_OTHER_CONTACTS_FAILED_MESSAGE = "Failed to list other contacts"
+GOOGLE_LIST_PEOPLE_FAILED_MESSAGE = "Failed to list people"
+GOOGLE_LIST_DIRECTORY_FAILED_MESSAGE = "Failed to list directory"
+GOOGLE_COPY_CONTACT_FAILED_MESSAGE = "Failed to copy contact"
+GOOGLE_GET_USER_PROFILE_FAILED_MESSAGE = "Failed to get user profile"
